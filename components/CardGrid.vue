08066e12903b139126534984ee5a1472a8366f9a
Daniel Aroucha <daniel.aroucha@icloud.com>
2024-05-20 18:11:25 -0300

    Vehicles view done

---
+++ components/CardGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="CardGrid" class="card-grid">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 16px;
+  width: 100%;
+}
+</style>
---
+++ components/PageWrapper.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="PageWrapper" class="page-wrapper">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.page-wrapper {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 0;
+  flex-shrink: 0;
+  gap: 72px;
+  width: 100%;
+}
+</style>
