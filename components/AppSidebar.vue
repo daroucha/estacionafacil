a47e6ba6b65213f4870679c9a747507ca5de02c3
Daniel Aroucha <daniel.aroucha@icloud.com>
2024-05-20 16:38:54 -0300

    AppSidebar done

---
+++ components/AppSidebar.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const links = [
+  {
+    label: 'Veículos',
+    icon: 'i-heroicons-truck',
+    to: '/vehicles'
+  },
+  {
+    label: 'Proprietários',
+    icon: 'i-heroicons-user-circle',
+    to: '/owners'
+  }
+]
+
+const computedYear = computed(() => {
+  const now = new Date()
+  return now.getFullYear()
+})
+</script>
+
+<template>
+  <div id="AppSidebar" class="sidebar bg-neutral-50 dark:bg-neutral-800">
+    <div class="header">
+      <img src="/img/logo.svg" alt="estacionafacil" />
+    </div>
+
+    <UVerticalNavigation :links="links" />
+
+    <div class="footer border-t border-neutral-200 dark:border-neutral-600">
+      <span class="text-xs leading-3 text-neutral-400">{{ computedYear }}, estacionafacil. Desenvolvido por Daniel
+        Aroucha.
+        <ULink to="https://github.com/daroucha/estacionafacil" target="_blank" active-class="text-primary"
+          inactive-class="text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200">GitHub ▶︎
+        </ULink>
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-between;
+  min-height: 100vh;
+  min-width: 180px;
+  padding-block: 32px;
+  padding-inline: 16px;
+  width: 14vw;
+}
+
+.sidebar>.header {
+  display: block;
+  width: 100%;
+}
+
+.sidebar>.header>img {
+  -webkit-user-drag: none;
+  display: block;
+  height: 14px;
+  width: auto;
+}
+
+.sidebar>.footer {
+  box-sizing: border-box;
+  padding-top: 24px;
+  width: 100%;
+  display: block;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const route = useRoute()
+
+useHead({
+  title: `${route.meta.title} - estacionafacil`,
+  meta: [
+    { property: 'og:title', content: `${route.meta.title} - estacionafacil` }
+  ]
+})
+</script>
+
+<template>
+  <div>
+    <AppSidebar />
+    <slot />
+  </div>
+</template>
