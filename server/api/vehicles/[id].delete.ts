bca2252528d2eeaa2d1479dea09710b69ddfa344
Daniel Aroucha <daniel.aroucha@icloud.com>
2024-05-22 14:02:34 -0300

    Vehicle delete done

--- server/api/vehicles/[id].delete.ts
+++ server/api/vehicles/[id].delete.ts
@@ -1,19 +1,18 @@
-// @desc    Update selected owner
-// @route   PUT /api/owners/:id
+// @desc    Delete one vehicle
+// @route   DELETE /api/vehicles/:id
 // @access  Private
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event)
-
   const { username } = await requireAuth(event)
 
   const user = await UserSchema.findOne({ username })
 
   try {
     if (user) {
-      return await OwnerSchema.findOneAndUpdate({
-        user: user._id,
-        _id: event.context.params?.id
-      }, body, { new: true })
+      await VehicleSchema.findByIdAndDelete(event.context.params?.id)
+
+      return {
+        success: true
+      }
     }
   } catch (error) {
     return error
